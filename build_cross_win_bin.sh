f38a4ed104b2fe3b186948eef0b49efe31a06fc9
wanjochan <wanjochan@gmail.com>
2018-09-16 22:54:43 +0800

    back to 1.5.0

--- build_cross_win_bin.sh
+++ build_cross_win_bin.sh
@@ -1,9 +1,12 @@
+# dkrun sh build_cross_win_bin.sh
+
 rm -Rf build_cross_win
 export CC='i386-win32-tcc -DTCC_TARGET_I386 -DTCC_TARGET_PE'
 #export CC='tcc -D__ANDROID__'
 mkdir build_cross_win
 cd build_cross_win
-cmake ..
+#cmake ..
+cmake -D CROSS=win ..
 make
 #upx dukluv
 
--- build_osx.sh
+++ build_osx.sh
@@ -2,7 +2,8 @@ rm -Rf build_osx
 #export CC='gcc'
 mkdir build_osx
 cd build_osx
-cmake ..
+#cmake ..
+cmake -D CROSS=osx ..
 make
 upx dukluv
 ./dukluv ../unit-tests.js
