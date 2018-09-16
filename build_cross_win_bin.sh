d23c2122017497513f74b2396583de354b582228
wanjochan <wanjochan@gmail.com>
2018-09-16 22:28:56 +0800

    TODO try using win32 tcc with correct headers...

--- build_cross_win_bin.sh
+++ build_cross_win_bin.sh
@@ -1,5 +1,5 @@
 rm -Rf build_cross_win
-export CC='i386-win32-tcc -DTCC_TARGET_I386 -DTCC_TARGET_P'
+export CC='i386-win32-tcc -DTCC_TARGET_I386 -DTCC_TARGET_PE'
 #export CC='tcc -D__ANDROID__'
 mkdir build_cross_win
 cd build_cross_win
